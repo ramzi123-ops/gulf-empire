7051a754f37a5cd3a52adbc5093b614986062157
Ramzi Haidan <ramzi.haidan.dev@gmail.com>
2025-11-01 00:03:55 +0300

    Implement password reset functionality with custom templates and email notifications

--- config/urls.py
+++ config/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('auth/', include('apps.users.urls')),
     path('', include('apps.store.urls')),
 ]
 

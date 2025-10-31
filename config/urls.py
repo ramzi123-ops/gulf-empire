fe8ad5fecfbfb607de15ba53912c47b47e440b86
Ramzi Haidan <ramzi.haidan.dev@gmail.com>
2025-11-01 00:24:26 +0300

    Add URL pattern for orders app to the main URL configuration

--- config/urls.py
+++ config/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('apps.users.urls')),
+    path('orders/', include('apps.orders.urls')),
     path('', include('apps.store.urls')),
 ]
 

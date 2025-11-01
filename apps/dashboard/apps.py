ad5ed876a405e8c2ef7e77d74eddee976c2f6ece
Ramzi Haidan <ramzi.haidan.dev@gmail.com>
2025-11-01 20:49:09 +0300

    Update verbose names for dashboard, inventory, orders, payments, store, and users apps to Arabic translations

--- apps/dashboard/apps.py
+++ apps/dashboard/apps.py
@@ -4,4 +4,4 @@ from django.apps import AppConfig
 class DashboardConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.dashboard'
-    verbose_name = 'Dashboard'
+    verbose_name = 'لوحة التحكم'

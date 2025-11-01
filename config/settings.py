0f1505de6c0231d007c1fb62d03f0933aa15df1e
Ramzi Haidan <ramzi.haidan.dev@gmail.com>
2025-11-01 19:29:26 +0300

    Add HTMX support by including django_htmx in INSTALLED_APPS and MIDDLEWARE

--- config/settings.py
+++ config/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    "django_htmx",
     # Our custom apps
     'apps.users',
     'apps.store',
@@ -59,6 +60,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django_htmx.middleware.HtmxMiddleware',  # HTMX middleware
 ]
 
 ROOT_URLCONF = 'config.urls'

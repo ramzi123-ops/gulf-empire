b6fbd5d3dddb8b321746fdc5303f6b4efd584965
Ramzi Haidan <ramzi.haidan.dev@gmail.com>
2025-11-02 21:09:17 +0300

    Edit admin name to gulf empire

--- config/urls.py
+++ config/urls.py
@@ -19,6 +19,11 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+# Customize Django Admin
+admin.site.site_header = 'إمبراطور الخليج - لوحة التحكم'
+admin.site.site_title = 'إمبراطور الخليج'
+admin.site.index_title = 'لوحة إدارة إمبراطور الخليج'
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('dashboard/', include('apps.dashboard.urls')),
